2c96ecdb6dc803e06678b8b719c7463afaa946cb
Ubuntu <ubuntu@ip-172-31-21-197.us-west-2.compute.internal>
2020-01-26 07:40:21 +0000

    reduced timesteps and changed logging

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # From the tensorflow/models/research/ directory
-PIPELINE_CONFIG_PATH="/home/ubuntu/dssg/data_utilities/vanilla_ddd.config"
-MODEL_DIR="/home/ubuntu/dssg/data_utilities/model"
-NUM_TRAIN_STEPS=50000
+PIPELINE_CONFIG_PATH="/home/ubuntu/dssg/model-training/vanilla_ddd.config"
+MODEL_DIR="/home/ubuntu/dssg/model-training/model"
+NUM_TRAIN_STEPS=3000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python ../tf/tensorflow/models/research/object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
     --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES \
-    --alsologtostderr
+    --alsolog
